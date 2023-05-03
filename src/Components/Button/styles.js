437e036441fb35979dba31c22bf0ba97985d83d1
Lucas Garcia <lucasgarciaamorim@gmail.com>
2023-05-03 19:15:37 -0300

    Desafio - Criando uma Calculadora com React - DIO

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+
+
+export const ButtonContainer = styled.button`
+
+padding:20px;
+border: 1px solig #CDCDCD;
+background-color: #0000FF;
+color: #FFFF;
+font-size: 22px;
+font-weight: 700;
+flex: 1;
+
+
+&:hover{
+    opacity: 0.7;
+    
+    transition: 0.3s;
+
+}
+
+`
+
---
+++ src/Components/Input/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  
+
+  display:flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size:24px;
+  font-family: 'roboto';
+  
+
+
+input{
+    width:100%;
+    height:75px;
+    background-color: #AAAFFF;
+    border:none;
+    text-align:right;
+    font-size: 3rem;
+    padding: 0 10px;
+
+
+
+}
+
+  
+`
+
